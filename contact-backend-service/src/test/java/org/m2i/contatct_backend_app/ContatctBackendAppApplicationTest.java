8195cd9fbf48736c45faa4c13b83c51d394c778d
Bazzou Mohammed <bazzoumohammed@gmail.com>
2024-06-28 22:04:29 +0200

    add project

---
+++ contact-backend-service/src/test/java/org/m2i/contatct_backend_app/ContatctBackendAppApplicationTest.java
@@ -0,0 +1,15 @@
+package org.m2i.contatct_backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatctBackendAppApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
